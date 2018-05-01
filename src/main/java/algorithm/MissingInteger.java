f2634dffca0aa254e5a6d59f045e75d1eafc1506
imjinbro <jinbro@likelion.org>
2018-05-01 10:26:31 +0900

    solved Counting Elements - MissingInteger(5/1)

---
+++ src/main/java/algorithm/MissingInteger.java
@@ -0,0 +1,34 @@
+package algorithm;
+
+/*
+    Counting Elements - https://app.codility.com/programmers/lessons/4-counting_elements/missing_integer/
+ */
+
+public class MissingInteger {
+    public static int solution(int[] A) {
+        boolean[] checker = new boolean[getMaxValue(A) + 1];
+        for (int num : A) {
+            if (num < 0 || checker[num] == true) {
+                continue;
+            }
+            checker[num] = true;
+        }
+
+        for (int i = 1; i < checker.length; i++) {
+            if (!checker[i]) {
+                return i;
+            }
+        }
+        return checker.length;
+    }
+
+    private static int getMaxValue(int[] A) {
+        int maxValue = 1;
+        for (int num : A) {
+            if (maxValue < num) {
+                maxValue = num;
+            }
+        }
+        return maxValue;
+    }
+}
