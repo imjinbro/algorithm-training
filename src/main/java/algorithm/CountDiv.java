8c2b34daedfe26c4686e8f43c609f770e68b232d
imjinbro <jinbro@likelion.org>
2018-05-04 23:11:25 +0900

    solved CountDiv(5/4)

---
+++ src/main/java/algorithm/CountDiv.java
@@ -0,0 +1,15 @@
+package algorithm;
+
+public class CountDiv {
+
+
+    public static int performanceFailSolution(int A, int B, int K) {
+        int cnt = 0;
+        for(int i = A; i <= B; i++) {
+            if (i % K == 0) {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+}
